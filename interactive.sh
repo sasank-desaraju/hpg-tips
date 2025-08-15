e21c11e0ec6619f656a6bb72ca6d8b2d8e49019c
Sasank Desaraju <sasank.desaraju@ufl.edu>
2025-08-15 13:02:22 -0400

    edited interactive script for new specs (B200)

--- interactive.sh
+++ interactive.sh
@@ -3,8 +3,8 @@ srun \
   --cpus-per-task=1 \
   --account=your-account-here \
   --qos=your-account-here \
-  --partition=gpu \
-  --gpus=a100:1 \
+  --job-name=InteractiveSrun \
+  --gpus=1 \
   --mem=8gb \
   --time=12:00:00 \
   --pty bash -i
@@ -12,3 +12,6 @@ srun \
 # Change the account and qos to your own account and qos
 # The account is the account name of the PI or the lab
 # The qos is the corresponding QOS (Quality of Service) name; usually same as account name
+
+# Include this line above gpus=1 if you want a B200 GPU instead of an L4 GPU.
+  # --partition=hpg-b200 \
